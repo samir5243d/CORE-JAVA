fa00391e2c9554259d65c2cda4c3c4f2842a56a7
samir5243d <samir5243d@gmail.com>
2025-06-26 12:03:31 +0530

    Updated [Loops] [new challenges added]

--- LOOPS/StudentChallenge1.java
+++ LOOPS/StudentChallenge1.java
@@ -2,32 +2,61 @@ import java.util.Scanner;
 
 public class StudentChallenge1 {
 
-     //DISPLAY MULTIPLICATION TABLE
-     public static void main(String[] args) {
+    // 1 //DISPLAY MULTIPLICATION TABLE
 
-        Scanner sc =new Scanner(System.in);
+    // public static void main(String[] args) {
 
-        System.out.println("Enter a no");
+    // Scanner sc =new Scanner(System.in);
 
-        int n=sc.nextInt();
+    // System.out.println("Enter a no");
 
-        for(int i=1; i<=10; i++){
-            System.out.println(i+"x"+i+"="+n*i);
+    // int n=sc.nextInt();
 
+    // for(int i=1; i<=10; i++){
+    // System.out.println(n+"x"+i+"="+n*i);
+
+    // }
+
+    // }
+
+    // // 2// FIND SUM OF NATURAL NUMBERS//
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+
+    // System.out.println("Enter a no");
+
+    // int n= sc.nextInt();
+
+    // int sum = 0;
+
+    // for(int i=1; i<=n; i++){
+
+    // sum=sum+i;
+
+    // }
+    // System.out.println("Sum of "+n+" Number is "+sum);
+
+    // }
+
+
+     //Factorial of a no//
+     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter a no");
+
+        int n = sc.nextInt();
+
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
         }
 
-        
-     }
+        System.out.println("factorial is " + factorial);
 
-    
+    }
 
-   
-
-    
-
-    
-   
-   
-
-    
 }
