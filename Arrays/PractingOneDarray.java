a88776d231331d93be72d79ceb27154052b5c978
samir5243d <samir5243d@gmail.com>
2025-07-10 16:00:09 +0530

    modifiiedarray

--- Arrays/PractingOneDarray.java
+++ Arrays/PractingOneDarray.java
@@ -1,4 +1,35 @@
 public class PractingOneDarray {
-    
-    
+    public static void main(String[] args) {
+        int A[]=new int[10];// Declaration and initialization of an array with size 10 
+
+        int B[]={1,2,3,4,5};// Declaration and initialization of an array with values
+
+        int C[];
+        C=new int[10];// Declaration of an array and then initialization with size 10
+
+        B[2]=15;
+
+
+
+        for(int i=0;i<B.length;i++){
+            
+            System.out.println(B[i]++);
+        }
+
+        for(int x:B){
+            System.out.println(x);
+        }
+
+        // for(int x:B){
+        //     System.out.println(x++);
+        // }
+
+        
+
+
+        
+    }
 }
+    
+    
+
