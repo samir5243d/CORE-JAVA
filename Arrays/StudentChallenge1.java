8849f816e80795c74fb62eae1f5b95a0baed9200
samir5243d <samir5243d@gmail.com>
2025-07-18 21:15:26 +0530

    added new challenge

--- Arrays/StudentChallenge1.java
+++ Arrays/StudentChallenge1.java
@@ -2,6 +2,27 @@
 
 public class StudentChallenge1 {
     public static void main(String[] args) {
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+
+        for(int x:A)
+        System.out.println(x+",");
+        System.out.println("");
+        
+
+        int temp=A[0];
+
+        for(int i=1; i<A.length; i++)
+        {
+              A[i-1]=A[i];
+        }
+
+        A[A.length-1]=temp;
+
+        for(int x:A)
+        System.out.println(x+",");
+        System.out.println("");
+
+    
         
     }
     
---
+++ Arrays/StudentChallenge2.java
@@ -0,0 +1,6 @@
+public class StudentChallenge2 {
+    
+    public static void main(String[] args) {
+        
+    }
+}
