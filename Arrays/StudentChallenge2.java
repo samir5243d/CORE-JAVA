eb20fd265688f284ffe96375fd9d07de2024af46
samir5243d <samir5243d@gmail.com>
2025-07-20 13:31:02 +0530

    added new shit

--- Arrays/StudentChallenge2.java
+++ Arrays/StudentChallenge2.java
@@ -1,6 +1,17 @@
+// INSERTING AN ELEMENT
+
 public class StudentChallenge2 {
     
     public static void main(String[] args) {
+
+        int A[]=new int [10];
+        A[0]=3; A[1]=9; A[2]=7; A[3]=8; A[4]=12; A[5]=6;
+
+
+        int n=6;
+
+    for(int i=0; i<n; i--)
+
         
     }
 }
