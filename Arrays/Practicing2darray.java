6430ef6b44636176e50c5363e0939605ef7f4740
samir5243d <samir5243d@gmail.com>
2025-07-24 16:45:09 +0530

    added new challenges

--- Arrays/Practicing2darray.java
+++ Arrays/Practicing2darray.java
@@ -43,22 +43,42 @@ public class Practicing2darray {
 
         // NOW JAGGED ARRAY BY USING FOR LOOP
 
-        int A[][];
+        // int A[][];
+        // A= new int [3] [];
+
+        // A[0]=new int[5];
+        // A[1]=new int[3];
+        // A[2]=new int[8];
+
+        // for(int i=0;i<A.length; i++)
+        // {
+        //     for(int j=0; j<A[i].length; j++)
+        //     {
+        //         System.out.print(A[i][j]+" ");
+        //     }
+        //     System.out.println("");
+        // }
+
+
+        // NOW JAGGED ARRAY BY USING FOR EACH LOOP
+
+   
+
+ int A[][];
         A= new int [3] [];
 
         A[0]=new int[5];
         A[1]=new int[3];
         A[2]=new int[8];
 
-        for(int i=0;i<A.length; i++)
+        for(int x[]:A)
         {
-            for(int j=0; j<A[i].length; j++)
+            for(int y:x)
             {
-                System.out.print(A[i][j]+" ");
+                System.out.print(y+" ");
             }
-            System.out.print("");
+            System.out.println("");
         }
-
 
 
 
