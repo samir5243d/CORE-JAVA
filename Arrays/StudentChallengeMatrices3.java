6b518defd450a3a6324cb2d4cc2120e81e7f2a1d
samir5243d <samir5243d@gmail.com>
2025-07-25 13:28:31 +0530

    added new matrices challanges in array

---
+++ Arrays/StudentChallengeMatrices3.java
@@ -0,0 +1,15 @@
+public class StudentChallengeMatrices3 {
+    
+
+    // SORTING ARRAY OF STRINGS
+
+    public static void main(String[] args) {
+        
+        String arr[]= {"java", "python", "pascal", "smalltalk", "ada", "basic"};
+
+        java.util.Arrays.sort(arr);
+
+        for(String x:arr)
+        System.out.println(x);
+    }
+}
