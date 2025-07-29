61cfb3e1fa07b8a3dff614c8dc1161b69a937abb
samir5243d <samir5243d@gmail.com>
2025-07-29 17:48:58 +0530

    learned new methods in java

--- Methods/PracticingObjectPassing.java
+++ Methods/PracticingObjectPassing.java
@@ -1,6 +1,20 @@
 public class PracticingObjectPassing {
+
+    static void change(int A[], int index, int value) {
+        A[index] = value;
+    }
+
     public static void main(String[] args) {
-            
+
+        int A[] = { 2, 4, 6, 8, 10 };
+
+        change(A,2,20);// Passing the array and modifying its element at index 2
+
+        for(int x:A){
+            System.out.println(x);
+        }
+
     }
 
 }
+
